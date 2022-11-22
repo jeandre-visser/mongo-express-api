cf648e6c8acf8b1f10b96369c76ddc84a247203e
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-22 15:09:22 -0700

    Build the get request for all users

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,25 @@
 import express from 'express';
 const router = express.Router();
+import User from '../models/user.js';
 
 // Get all users
-router.get('/', (req, res) => {
-  
-})
-// Get one user
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch(err) {
+    res.status(500).json({ message: err.message })
+  }
 })
 
 // Create a user
 router.post('/', (req, res) => {
-  
+
+})
+
+// Get one user
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
 })
 
 // Update a user
