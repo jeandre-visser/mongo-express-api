16a9dac829675185e9d078a0951eae30153d8c38
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-22 15:27:53 -0700

    Create an async function to getUser to use in other router handlers

--- routes/users.js
+++ routes/users.js
@@ -44,5 +44,24 @@ router.delete('/:id', (req, res) => {
   
 })
 
+async function getUser(req, res, next) {
+  let user;
+  try {
+    user = await User.findById(req.params.id);
+    if (user == null) {
+      // Could not find user
+      return res.status(404).json({ message: 'User not found' })
+    }
+  } catch (err) {
+    // Error on server side
+    return res.status(500).json({ message: err.message })
+  }
+  // res.user can now be used in our other router handlers
+  res.user = user;
+
+  // Moves onto next piece of middleware or request
+  next();
+}
+
 // Export router to use in index.js
 export default router;
