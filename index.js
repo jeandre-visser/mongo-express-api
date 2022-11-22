f805d32bb093264aa9465679e37b9e69afb78b2f
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-22 14:42:33 -0700

    Import usersRouter and use

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import * as dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 dotenv.config();
+import usersRouter from './routes/users.js';
 
 const app = express();
 const PORT = 8080;
@@ -14,6 +15,8 @@ mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log("Connected to mongo database"));
+
+app.use('/users', usersRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
