f5f6e5d6efe8fe60e2e1a0ef839394da74e74fa1
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-22 14:51:35 -0700

    Create basic shell fo reach route

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,28 @@ import express from 'express';
 const router = express.Router();
 
 // Get all users
-
+router.get('/', (req, res) => {
+  
+})
 // Get one user
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+})
 
 // Create a user
+router.post('/', (req, res) => {
+  
+})
 
 // Update a user
+router.patch('/:id', (req, res) => {
+  
+})
 
 // Delete a user
+router.delete('/:id', (req, res) => {
+  
+})
 
 // Export router to use in index.js
 export default router;
