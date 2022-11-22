de15fe848e8b1f0a8267c67d05fdf61fa27db379
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-22 14:35:05 -0700

    Coonnect to mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import express from "express";
 const app = express();
 const PORT = 8080;
+import mongoose from "mongoose";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log("Connected to mongo database"));
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
