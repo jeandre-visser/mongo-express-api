4fa5e70e7a2e04f44e6d8a82b68f7eb0dc9e7990
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-22 14:49:19 -0700

    File for user routes

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+import express from 'express';
+const router = express.Router();
+
+// Get all users
+
+// Get one user
+
+// Create a user
+
+// Update a user
+
+// Delete a user
+
+// Export router to use in index.js
+export default router;
